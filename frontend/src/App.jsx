7a7a93c01ba828043543e172fcd1307617db1920
hermeskongo <hermeskongo@gmail.com>
2026-01-17 11:40:01 +0100

    setup application routing: Création des différents composants de page et gestion de routing entre ces pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import {LoginPage} from "./pages/LoginPage.jsx";
+import {OnboardingPage} from "./pages/OnboardingPage.jsx";
+import {ChatPage} from "./pages/ChatPage.jsx";
+import {CallPage} from "./pages/CallPage.jsx";
+
 export const  App = () => {
 
   return (
-    <>
-    </>
+    <div className="h-screen font-sans" data-theme="night">
+      <Routes>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/onboarding" element={<OnboardingPage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
+        <Route path="/call" element={<CallPage/>}/>
+      </Routes>
+    </div>
   )
 }
 
