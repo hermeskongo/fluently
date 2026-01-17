bce01293488fca5e7a1dda24170c641fe8955afd
hermeskongo <hermeskongo@gmail.com>
2026-01-17 13:08:25 +0100

    feat: ajouter la gestion de l'authentification avec react-query, protections des routes et gestion de cors error(backend).

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser"
 import {userRoutes} from "./Routes/user.routes.js";
 import {chatCodes} from "stream-chat";
 import {chatRoutes} from "./Routes/chat.routes.js";
+import cors from "cors"
 
 const app = express()
 
@@ -15,6 +16,14 @@ testDrizzle()
 
 app.use(express.json())
     .use(cookieParser())
+    .use(cors({
+        origin: [
+            "http://localhost:5174",
+            "http://localhost:5173",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }))
 
 app.use('/api/v1/auth', authRoutes)
     .use('/api/v1/users', userRoutes)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,37 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {HomePage} from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import {LoginPage} from "./pages/LoginPage.jsx";
 import {OnboardingPage} from "./pages/OnboardingPage.jsx";
 import {ChatPage} from "./pages/ChatPage.jsx";
 import {CallPage} from "./pages/CallPage.jsx";
+import {useQuery} from "@tanstack/react-query";
+import {axiosInstance} from "./lib/axios.js";
+import {API_PATHS} from "./lib/constant.js";
 
 export const  App = () => {
+
+  const {data:authData, isLoading, error} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get(API_PATHS.AUTH.GET_USER)
+
+      return res.data
+    },
+    retry: false
+  })
+
+  const authUser = authData?.user
 
   return (
     <div className="h-screen font-sans" data-theme="night">
       <Routes>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/onboarding" element={<OnboardingPage/>}/>
-        <Route path="/chat" element={<ChatPage/>}/>
-        <Route path="/call" element={<CallPage/>}/>
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+        <Route path="/onboarding" element={authUser ? <OnboardingPage/> : <Navigate to="/login"/>}/>
+        <Route path="/chat" element={authUser ? <ChatPage/> : <Navigate to="/login"/>}/>
+        <Route path="/call" element={authUser ? <CallPage/> : <Navigate to="/login"/>}/>
       </Routes>
     </div>
   )
