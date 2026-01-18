b39191d92ca91ba3202dc81df69809b27b997ae4
hermeskongo <hermeskongo@gmail.com>
2026-01-18 08:37:37 +0100

    refactor: réorganiser les importations et supprimer une URL de CORS obsolète

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
-import 'dotenv/config'
-import express from "express"
-import {testDrizzle} from "./Config/db/drizzle.js";
-import {authRoutes} from "./Routes/auth.routes.js";
-import cookieParser from "cookie-parser"
-import {userRoutes} from "./Routes/user.routes.js";
-import {chatRoutes} from "./Routes/chat.routes.js";
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import 'dotenv/config';
+import express from "express";
+import { testDrizzle } from "./Config/db/drizzle.js";
+import { authRoutes } from "./Routes/auth.routes.js";
+import { chatRoutes } from "./Routes/chat.routes.js";
+import { userRoutes } from "./Routes/user.routes.js";
 
 const app = express()
 
@@ -20,7 +20,6 @@ app
         origin: [
             "http://localhost:5174",
             "http://localhost:5173",
-            "https://wild-paths-smash.loca.lt"
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
