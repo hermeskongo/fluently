584ae5f971a665209f2291dcdfa91c44378d6d40
hermeskongo <hermeskongo@gmail.com>
2026-01-19 05:31:18 +0100

    prod: try to fix api error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL
 
+console.log(import.meta.env.VITE_BACKEND_URL)
+
 export const axiosInstance = axios.create({
     baseURL: BACKEND_BASE_URL,
     withCredentials: true
